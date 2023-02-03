aec181117681009e86b531dcf11ebf0672f74937
ugochukwudev <paulambrose5002@gmail.com>
2023-02-03 22:33:50 +0100

    meta image

--- app/(user)/head.tsx
+++ app/(user)/head.tsx
@@ -4,7 +4,10 @@ export default function Head() {
       <title>Rumour Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="rumour blog" />
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        href="https://techiesportfolio.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimg.149fb20c.jpeg&w=2048&q=75"
+      />
     </>
   );
 }
